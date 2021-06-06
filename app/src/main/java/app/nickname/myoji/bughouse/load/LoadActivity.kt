50ef88dad473b1587643f31dfd0e80fd20fe4b76
risako070310 <risako070310@gmail.com>
2021-06-06 11:23:35 +0900

    count, list, save, load

--- app/src/main/java/app/nickname/myoji/bughouse/load/LoadActivity.kt
+++ app/src/main/java/app/nickname/myoji/bughouse/load/LoadActivity.kt
@@ -1,19 +1,20 @@
 package app.nickname.myoji.bughouse.load
 
 import android.content.Context
+import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import app.nickname.myoji.bughouse.R
 
 class LoadActivity : AppCompatActivity() {
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_load)
 
         val textView: TextView = findViewById(R.id.text_view)
 
-        val sharedPreferences = getSharedPreferences("Third", Context.MODE_PRIVATE)
+        val sharedPreferences = getSharedPreferences("Second", Context.MODE_PRIVATE)
         textView.text = sharedPreferences.getString("save", "")
     }
 }
